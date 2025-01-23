9dc8b9f48cf6d00b67ec62aa34d6b5135fd0924a
noah <qny123@naver.com>
2025-01-23 12:57:46 +0900

    fix: 모바일 배포 환경에서 svg이미지 깨지는 문제 해결

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["kream-phinf.pstatic.net"],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
